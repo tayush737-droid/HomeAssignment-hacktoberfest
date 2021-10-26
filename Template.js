89acf2f8d258d1cadf2f7e24bc2339dcca2e553a
prasannaelectrotech <67099812+prasannaelectrotech@users.noreply.github.com>
2021-10-26 20:16:20 +0530

    Update Template.js

--- Template.js
+++ Template.js
@@ -2,7 +2,29 @@ var api_url = '';
 // var api_url = 'http://127.0.0.1:5000/';
 
 function deleteCookies(cname, cvalue, exdays) {
-	setCookie(cname, cvalue, exdays);
+	setCookie(cname, cvalue, exdays);st');
+				resolve(returnObj);
+			},
+			(error) => {
+				reject(error);
+			}
+		);
+	});
+}
+
+function black_box_test() {
+	window.alert('Black-box testing is a method of software testing that examines the functionality of an application without peering into its internal structures or workings. This method of test can be applied virtually to every level of software testing: unit, integration, system and acceptance.');
+}
+
+function if_validateEmail(email) {
+	window.alert('validating email');
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+
+
+function makeGetRequest(path) {
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -40,29 +62,7 @@ function makeAsyncPostRequest(path, queryObject) {
 		axios.post(api_url + path, queryObject).then(
 			(response) => {
 				var returnObj = response.data;
-				console.log('Async Post Request');
-				resolve(returnObj);
-			},
-			(error) => {
-				reject(error);
-			}
-		);
-	});
-}
-
-function black_box_test() {
-	window.alert('Black-box testing is a method of software testing that examines the functionality of an application without peering into its internal structures or workings. This method of test can be applied virtually to every level of software testing: unit, integration, system and acceptance.');
-}
-
-function if_validateEmail(email) {
-	window.alert('validating email');
-	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
-}
-
-
-
-function makeGetRequest(path) {
+				console.log('Async Post Reque
 	axios.get(api_url + path).then(
 		(response) => {
 			var returnObj = response.data;
