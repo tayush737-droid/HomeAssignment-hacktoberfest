282118fc006626e6cdb51aa00381e4b34b31ab5d
Chitrank Mishra <56322586+chitrank0614@users.noreply.github.com>
2022-10-08 15:28:51 +0530

    Update reach.cpp

--- reach.cpp
+++ reach.cpp
@@ -17,3 +17,155 @@ int main(){
 }ing namespace std;
 out<<y<<"\n";
 }
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
+#include <iostream>
+us
+int y = 0;
+int fu
+	}
+	c
+
+int main(){
+	int x = 6;
+	fn1(int x)
+{
+
+	if(x>0){
+		fun1(x-1);
+		cout<<x<<"\n";
+		y++;un1(x);
+}ing namespace std;
+out<<y<<"\n";
+}
